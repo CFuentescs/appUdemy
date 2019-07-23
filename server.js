3a29d92eae36d74a5840153cf77503e68f3ddf5c
carlos <c.fuentescorts@gmail.com>
2019-07-23 11:04:50 -0700

    udemy nodejs

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
+var hbs = require('hbs');
 require('./hbs/helpers');
 
-var hbs = require('hbs');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
